eba7678e6e5c8ee6617bf5c65cf0c97d4c6448ed
lakshmiv9829 <76515530+lakshmiv9829@users.noreply.github.com>
2021-01-25 01:33:39 +0530

    Add files via upload

---
+++ SingleTonTester.java
@@ -0,0 +1,10 @@
+package com.xworkz.designPattern;
+
+public class SingleTonTester {
+	public static void main(String[] args) {
+
+		System.out.println(EagerInitSingleton.getinstance());
+		
+	}
+
+}
